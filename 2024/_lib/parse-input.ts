eaea2edf4be61a4033e0e0b26c7bda45f9e2f337
Josh Payette <hello@joshpayette.dev>
2024-12-02 08:24:06 -0500

    Updated parser to remove empty lines

--- 2024/_lib/parse-input.ts
+++ 2024/_lib/parse-input.ts
@@ -4,5 +4,8 @@ import { join } from 'path';
 // Parses the input.txt file for a specific puzzle and returns the data
 export function parseInput(path: string): string[] {
   const input = readFileSync(join(__dirname, `../${path}`), 'utf-8');
-  return input.split('\n').map((line) => line.trim());
+  return input
+    .split('\n')
+    .map((line) => line.trim())
+    .filter((line) => line.length > 0);
 }
